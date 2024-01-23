7a0b95b9cdcb4255b641c8f2e99bb93cdd671dfe
NaialMack <naialmc@hotmail.com>
2024-01-23 00:05:35 -0500

    Commit message

--- candidate-testing.js
+++ candidate-testing.js
@@ -20,17 +20,17 @@ let candidateAnswers = [];
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
-  for( i = 0; i < questions.length; i++) {
-    answer = input.question(questions[i]) 
-    candidateAnswers.push(answer)
-  } 
+candidateName = input.question("Enter name: ");
 
 } 
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-candidateAnswer = input.question(question);
+
+  for( i = 0; i < questions.length; i++) {
+    answer = input.question(questions[i]) 
+    candidateAnswers.push(answer)
+  } 
 
 }
 
